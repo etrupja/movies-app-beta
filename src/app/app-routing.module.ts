f9d045522b1bc94d2929e0b4f39d9f0475763657
Ervis Trupja <ervis.trupja@projekt202.com>
2022-07-10 17:23:57 +0200

    child components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,20 +10,19 @@ import { DetailsReviewsComponent } from './details-reviews/details-reviews.compo
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:title', component: SearchComponent },
-  { path: 'details/:id', component: DetailsComponent },
-  { path: 'details/:id/actors', component: DetailsActorsComponent },
-  { path: 'details/:id/reviews', component: DetailsReviewsComponent },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    children: [
+      { path: 'actors', component: DetailsActorsComponent },
+      { path: 'reviews', component: DetailsReviewsComponent },
+    ],
+  },
 ];
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
