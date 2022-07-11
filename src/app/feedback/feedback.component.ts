c478895357937d429b83c5c9c75393da4019731a
Ervis Trupja <ervis.trupja@projekt202.com>
2022-07-11 23:46:40 +0200

    feedback

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-feedback',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class FeedbackComponent implements OnInit {
   feedback: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  constructor() {}
+  constructor(public auth: AuthService) {}
 
   ngOnInit(): void {}
 }
